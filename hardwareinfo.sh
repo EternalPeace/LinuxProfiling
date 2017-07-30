fa298434d9334265f3b18897f806afd4a740e902
root <root@localhost.localdomain>
2017-07-30 09:35:20 -0400

    1

--- hardwareinfo.sh
+++ hardwareinfo.sh
@@ -1,5 +1,7 @@
 
 #get the CPU info
+mkdir hwinfo
+cd hwinfo
 cores=`cat /proc/cpuinfo | grep "model name" | wc -l`
 echo "cores = " $cores >> hwinfo.log
 cpuinfo=`cat /proc/cpuinfo | grep "model name" | uniq`
